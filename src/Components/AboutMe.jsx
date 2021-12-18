f727955a8355bfa9179787ad25519557f0559224
B3HI13 <behnamsa96@outlook.com>
2021-12-18 19:11:01 +0330

    Width classes added

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function AboutMe() {
   return (
-  <div className="bg-slate-600">
+  <div className="bg-slate-500 w-24 sm:w-40 md:w-48 lg:w-64">
       Behnam SeydAbadi
   </div>);
 }
