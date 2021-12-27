174dabc46a4da981caad121262b9d9ba39266cc4
B3HI13 <behnamsa96@outlook.com>
2021-12-27 19:07:15 +0330

    ImageTemplate added to image component

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -5,6 +5,7 @@ function AboutMe() {
   return (
     <React.Fragment>
       <Image
+        isCircle={true}
         imageUrl={"/Images/userImage.svg"}
         width={512}
         height={512}
--- src/Components/Common/Image.jsx
+++ src/Components/Common/Image.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "../../Styles/Image.css";
 
-
-
-function Image({ imageUrl, width, height, alt }) {
+function Image({ isCircle, imageUrl, width, height, alt }) {
   console.log(imageUrl);
 
+  let imageTemplate;
+
+  if (isCircle) imageTemplate = "circle";
+
   return (
-    <img className="circle border border--white" src={imageUrl} width={width} height={height} alt={alt} />
+    <img
+      className={`${imageTemplate} border border--white`}
+      src={imageUrl}
+      width={width}
+      height={height}
+      alt={alt}
+    />
   );
 }
 
