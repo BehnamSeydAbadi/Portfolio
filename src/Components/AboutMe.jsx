8fc7a61991d7c878e5a333599c01c2e949b65352
B3HI13 <behnamsa96@outlook.com>
2021-12-28 18:47:34 +0330

    Title and about me text added to AboutMe component

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import Image from "./Common/Image";
 import { ImageTemplateEnum } from "../Constants/ImageTemplateEnum.js";
 
-function AboutMe() {
+function AboutMe({ Name, LastName, summary }) {
   return (
-    <React.Fragment>
+    <div className="flex flex-col justify-center items-center">
       <Image
         imageTemplate={ImageTemplateEnum.CIRCLE}
         imageUrl={"/Images/userImage.svg"}
@@ -12,7 +12,13 @@ function AboutMe() {
         height={512}
         alt={"XXX"}
       />
-    </React.Fragment>
+
+      <h1>
+        <span className="text-5xl uppercase tracking-widest">{Name}</span>
+        <span className="text-5xl uppercase tracking-widest font-bold">{LastName}</span>
+      </h1>
+      <p>{summary}</p>
+    </div>
   );
 }
 
