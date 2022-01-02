5e3a41eb898d0d50fbe1260f6307854d4ab38511
B3HI13 <behnamsa96@outlook.com>
2022-01-02 18:09:01 +0330

    Required fonts added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
     extend: {
       spacing: {
         "15": "3.75rem",
-        "128" : "32rem"
+        "128": "32rem"
+      },
+      fontFamily: {
+        'Roboto': 'Roboto, sans-serif',
+        'OpenSans':'Open Sans, sans-serif',
+        'Montserrat':'Montserrat, sans-serif'
       },
     },
   },
