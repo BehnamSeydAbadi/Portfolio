ab8f90134b430518691eef7b5c69bba07d24fb91
Behnam S.A <behnamsa96@outlook.com>
2022-01-07 00:53:24 +0330

    Image name and last name responsive added

--- src/Components/AboutMe.jsx
+++ src/Components/AboutMe.jsx
@@ -4,7 +4,7 @@ import "../Styles/AboutMe.css";
 function AboutMe({ name, lastName, summary }) {
   const wrapperDivCenterAlignmentClasses = "flex flex-col justify-center items-center";
   const wrapperDivSizeClasses = "h-[51rem]";
-  const wrapperDivGradientClasses = "bg-gradient-hardEdge";
+  const wrapperDivGradientClasses = "bg-gradient-hardEdge-mobile sm:bg-gradient-hardEdge";
   const wrapperDivButtomMarginClass = "mb-1";
 
   return (
@@ -14,12 +14,16 @@ function AboutMe({ name, lastName, summary }) {
       ${wrapperDivGradientClasses} ${wrapperDivButtomMarginClass}`}>
 
       <img
-        className={`mt-[-4rem] rounded-full border-8 border-white w-[324px] h-[324px] sm:w-[422px] sm:h-[422px]`}
+        className={`
+          mt-[-9rem] sm:mt-[-4rem] 
+          rounded-full 
+          border-8 border-white
+          w-[324px] h-[324px] sm:w-[422px] sm:h-[422px]`}
         src="/Images/userImage.svg"
         alt="User's photo"
       />
 
-      <div className='mt-15 text-5xl font-Roboto tracking-wider uppercase'>
+      <div className='text-2xl mt-14 ms:mt-15 ms:text-5xl font-Roboto tracking-wider uppercase'>
         {name} <b>{lastName}</b>
       </div>
       <p className='mt-5 w-128 text-base text-center font-OpenSans tracking-wide'>
