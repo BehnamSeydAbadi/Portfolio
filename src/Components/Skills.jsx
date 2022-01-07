b13484f90884da2c5b4ebb59d21cb50a8b16a7a4
Behnam S.A <behnamsa96@outlook.com>
2022-01-07 13:56:00 +0330

    Title classes added to skills component

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 
 function Skills() {
+
+  const titleClasses = "sm:mt-[6rem] text-center sm:text-7xl font-bold";
+
   return <div className="flex bg-white sm:h-128">
     <div name="left" className="flex-initial w-1/2">
-      <div className="text-center text-5xl font-bold text-gray-400 mt-18">Back-End</div>
+      <div className={`${titleClasses} text-gray-400`}>Back-End</div>
     </div>
-    <div name="right"className="flex-initial w-1/2">right</div>
+    <div name="right" className="flex-initial w-1/2">
+      <div className={`${titleClasses} text-orange-400`}>Front-End</div>
+    </div>
   </div>;
 }
 
