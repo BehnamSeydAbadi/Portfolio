54dadb43cf96afd71b250fd52443d7159dab0ec8
Behnam S.A <behnamsa96@outlook.com>
2022-01-07 14:17:43 +0330

    Ul Li added

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -4,14 +4,22 @@ function Skills() {
 
   const titleClasses = "sm:mt-[6rem] text-center sm:text-7xl font-bold";
 
+  const backEndSkills = ["ASP.NET Core", "TDD", "DDD", "CQRS", "SQL", "NoSQL"];
+
   return <div className="flex bg-white sm:h-128">
-    <div name="left" className="flex-initial w-1/2">
-      <div className={`${titleClasses} text-gray-400`}>Back-End</div>
+  <div name="left" className="flex-initial w-1/2">
+    <div className={`${titleClasses} text-gray-400`}>Back-End</div>
+    <div name="backEndSkillsList" className="mt-8 flex-">
+      <ul>
+        {backEndSkills.map(skill => <li className="text-2xl">{skill}</li>)}
+      </ul>
     </div>
-    <div name="right" className="flex-initial w-1/2">
-      <div className={`${titleClasses} text-orange-400`}>Front-End</div>
-    </div>
-  </div>;
+
+  </div>
+  <div name="right" className="flex-initial w-1/2">
+    <div className={`${titleClasses} text-orange-400`}>Front-End</div>
+  </div>
+</div>;
 }
 
 export default Skills;
