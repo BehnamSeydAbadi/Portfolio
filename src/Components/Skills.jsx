7958771c1f52330897f2002c05d05f8a15ffcf11
Behnam S.A <behnamsa96@outlook.com>
2022-01-07 13:49:52 +0330

    Classes added to left div

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
 function Skills() {
-  return <div className="flex">
-    <div name="left" className="flex-initial w-1/2">left</div>
+  return <div className="flex bg-white sm:h-128">
+    <div name="left" className="flex-initial w-1/2">
+      <div className="text-center text-5xl font-bold text-gray-400 mt-18">Back-End</div>
+    </div>
     <div name="right"className="flex-initial w-1/2">right</div>
   </div>;
 }
