060e45ee2159c9115de63c695a8e612508fb984d
B3HI13 <behnamsa96@outlook.com>
2022-01-10 18:44:46 +0330

    Cadmium orange color added to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,15 +3,18 @@ module.exports = {
   theme: {
     extend: {
       spacing: {
-        "15": "3.75rem",
-        "128": "32rem"
+        15: "3.75rem",
+        128: "32rem",
       },
       fontFamily: {
-        'Roboto': 'Roboto, sans-serif',
-        'OpenSans':'Open Sans, sans-serif',
-        'Montserrat':'Montserrat, sans-serif'
-      }
-    }
+        Roboto: "Roboto, sans-serif",
+        OpenSans: "Open Sans, sans-serif",
+        Montserrat: "Montserrat, sans-serif",
+      },
+    },
+    colors: {
+      cadmiumOrange: "#fb923c",
+    },
   },
   plugins: [],
 };
