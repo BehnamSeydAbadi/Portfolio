c97eace5838276cb240789829a6e880059433f17
B3HI13 <behnamsa96@outlook.com>
2022-01-10 18:56:29 +0330

    transition duration fixed

--- src/Components/Samples.jsx
+++ src/Components/Samples.jsx
@@ -2,10 +2,11 @@ import React from "react";
 
 function Samples({ samplesShot }) {
   return (
-    <div className='grid grid-cols-none md:grid-cols-2 lg:grid-cols-3'>
+    <div className='bg-cadmiumOrange grid grid-cols-none md:grid-cols-2 lg:grid-cols-3'>
       {samplesShot.map((sampleShot) => (
-        <a className='transition hover:opacity-0' href={sampleShot.href}>
+        <a href={sampleShot.href}>
           <img
+            className='transition duration-500 hover:opacity-0'
             src={sampleShot.path}
             alt={sampleShot.alt}
           />
