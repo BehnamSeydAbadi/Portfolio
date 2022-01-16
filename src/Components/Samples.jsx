0b591d93d5ee9dcdfa5219eae9456ce963b1bfd4
B3HI13 <behnamsa96@outlook.com>
2022-01-16 18:10:43 +0330

    Background text added to samples grid column

--- src/Components/Samples.jsx
+++ src/Components/Samples.jsx
@@ -4,12 +4,14 @@ function Samples({ samplesShot }) {
   return (
     <div className='bg-cadmiumOrange grid grid-cols-none md:grid-cols-2 lg:grid-cols-3'>
       {samplesShot.map((sampleShot) => (
-        <a href={sampleShot.href}>
+        <a className='relative flex justify-center items-center' href={sampleShot.href}>
           <img
             className='transition duration-500 hover:opacity-0'
             src={sampleShot.path}
             alt={sampleShot.alt}
           />
+          
+          <h1 className='absolute'>BBBGBBB</h1>
         </a>
       ))}
     </div>
